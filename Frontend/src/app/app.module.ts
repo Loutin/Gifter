3a605257cd63221a0e2a382d810deb5d7113f4ce
Loutin <fatimaagustin33@hotmail.com>
2023-11-02 19:55:24 -0300

    Todoas las paginas terminadas

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { BackPageComponent } from './back-page/back-page.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { RegisterComponent } from './register/register.component';
     FooterComponent,
     NosotrosComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    BackPageComponent
   ],
   imports: [
     BrowserModule,
